98fad920b9bdd7d1298f4eb26ef873a846898174
sadeqsoli <solisadeq@gmail.com>
2023-08-17 19:02:29 +0330

    Adding Sort panel and Finishing with Selection panel.
    Selection panel has a series of buttons that each on follow different method for choosing a domain for number selection.
    Sort panel is the main one that use a bunch of buttons to invoke different methods of sorting, which at this moment is not complete and only displays the random numbers that are generated. all of it happened in Sample scene and sort manager class.

--- Sort_DS/Assets/Scripts/Managers/SortManager.cs
+++ Sort_DS/Assets/Scripts/Managers/SortManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,34 @@ using UnityEngine.UI;
 
 public class SortManager : MonoBehaviour
 {
-    [SerializeField] Button _mAddNumbers;
+    [SerializeField] Numb NumberPrefab;
+    [SerializeField] Transform ParentTR;
 
+    List<int> _mCurrentSelectedList;
 
-
-    private void Start()
+    private void Awake()
     {
-        
+        EventManager.ListSenderEvent?.AddListener(InitializeSettingUp);
     }
+
+    private void InitializeSettingUp(List<int> listOfNumbers)
+    {
+        _mCurrentSelectedList = listOfNumbers;
+
+        foreach (var number in listOfNumbers)
+        {
+            var numbInstance = Instantiate(NumberPrefab);
+            numbInstance.transform.SetParent(ParentTR);
+            numbInstance.transform.localPosition = Vector3.zero;
+            numbInstance.transform.localScale = Vector3.one;
+            numbInstance.InitNumber(number);
+        }
+
+    }
+    private void OnDisable()
+    {
+        EventManager.ListSenderEvent?.RemoveListener(InitializeSettingUp);
+    }
+
 }
 
