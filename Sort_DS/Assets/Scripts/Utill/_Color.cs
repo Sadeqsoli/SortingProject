27efd8c4d1beb8717cce898d28b3c1f0be856b32
sadeqsoli <solisadeq@gmail.com>
2023-08-17 19:08:53 +0330

    Adding _Color Class for easy coloring .

---
+++ Sort_DS/Assets/Scripts/Utill/_Color.cs
@@ -0,0 +1,405 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+
+public static class _Color
+{
+    #region Properties
+    public static Color Red
+    {
+        get
+        {
+            Color redColor = new Color(0.8f, 0.03f, 0f, 1f);
+            return redColor;
+        }
+    }
+    public static Color R_SLRed
+    {
+        get
+        {
+            Color SLRedColor = new Color(1f, 0.4f, 0.4f, 1f);
+            return SLRedColor;
+        }
+    }
+    public static Color R_FalseRed
+    {
+        get
+        {
+            Color falseRedColor = new Color(1f, 0.2941177f, 0.2941177f, 1f);
+            return falseRedColor;
+        }
+    }
+    public static Color R_Orange
+    {
+        get
+        {
+            Color orangeColor = new Color(1f, 0.6470588f, 0f, 1f);
+            return orangeColor;
+        }
+    }
+    public static Color R_DRed
+    {
+        get
+        {
+            Color darkeRedColor = new Color(0.5764706f, 0f, 0f, 1f);
+            return darkeRedColor;
+        }
+    }
+    public static Color R_DOrange
+    {
+        get
+        {
+            Color darkOrangeColor = new Color(1f, 0.5490196f, 0f, 1f);
+            return darkOrangeColor;
+        }
+    }
+    public static Color R_DMagenta
+    {
+        get
+        {
+            Color darkMagentaColor = new Color(0.8f, 0f, 0.25f, 1f);
+            return darkMagentaColor;
+        }
+    }
+
+
+    public static Color P_SDPurple
+    {
+        get
+        {
+            Color superDarkPurpleColor = new Color(0.3254902f, 0.1098039f, 0.3607843f, 1f);
+
+            return superDarkPurpleColor;
+        }
+    }
+    public static Color Purple
+    {
+        get
+        {
+            Color purpleColor = new Color(0.6705883f, 0.2078431f, 0.9019608f, 1f);
+
+            return purpleColor;
+        }
+    }
+    public static Color P_LPurple
+    {
+        get
+        {
+            Color LpurpleColor = new Color(0.7843137f, 0.4705882f, 1f, 1f);
+
+            return LpurpleColor;
+        }
+    }
+   
+
+
+    public static Color Blue
+    {
+        get
+        {
+            Color BlueColor = new Color(0.1084016f, 0.22241f, 0.7924528f, 1f);
+            return BlueColor;
+        }
+    }
+    public static Color B_LBlue
+    {
+        get
+        {
+            Color LBlueColor = new Color(0.47f, 0.8f, 1f, 1f);
+            return LBlueColor;
+        }
+    }
+    public static Color B_SDBlue
+    {
+        get
+        {
+            Color SDBlueColor = new Color(0f, 0.01715729f, 0.3490566f, 1f);
+            return SDBlueColor;
+        }
+    }
+
+
+    public static Color T_LTurquoise
+    {
+        get
+        {
+            Color LTurquoiseColor = new Color(0.4f, 1f, 1f, 1f);
+            return LTurquoiseColor;
+        }
+    }
+    public static Color Turquoise
+    {
+        get
+        {
+            Color TurquoiseColor = new Color(0f, 1f, 1f, 1f);
+            return TurquoiseColor;
+        }
+    }
+    public static Color T_DTurquoise
+    {
+        get
+        {
+            Color DTurquoiseColor = new Color(0f, 0.6f, 0.6f, 1f);
+            return DTurquoiseColor;
+        }
+    }
+    public static Color T_SDTurquoise
+    {
+        get
+        {
+            Color SDTurquoiseColor = new Color(0f, 0.4f, 0.4f, 1f);
+            return SDTurquoiseColor;
+        }
+    }
+
+
+
+
+    public static Color Y_SLYellow
+    {
+        get
+        {
+            Color SLyellowColor = new Color(1f, 1f, 0.6666667f, 1f);
+            return SLyellowColor;
+        }
+    }
+    public static Color Y_SSLYellow
+    {
+        get
+        {
+            Color superLightYellowColor;
+            if (ColorUtility.TryParseHtmlString("#FFEE79", out superLightYellowColor))
+            {
+            }
+            return superLightYellowColor;
+        }
+    }
+
+    public static Color Y_LYellow
+    {
+        get
+        {
+            Color yellowColor = new Color(1f, 0.8375235f, 0.3915094f, 1f);
+            return yellowColor;
+        }
+    }
+    public static Color Yellow
+    {
+        get
+        {
+            Color yellowColor = new Color(1f, 0.8318377f, 0f, 1f);
+            return yellowColor;
+        }
+    }
+    public static Color Y_Gold
+    {
+        get
+        {
+            Color goldColor = new Color(1f, 0.8431373f, 0f, 1f);
+            return goldColor;
+        }
+    }
+    public static Color Y_LOlive
+    {
+        get
+        {
+            Color lightOliveColor = new Color(0.7450981f, 0.7450981f, 0f, 1f);
+            return lightOliveColor;
+        }
+    }
+    public static Color Y_Olive
+    {
+        get
+        {
+            Color oliveColor = new Color(0.5019608f, 0.5019608f, 0f, 1f);
+            return oliveColor;
+        }
+    }
+    public static Color Y_Brown
+    {
+        get
+        {
+            Color brownColor = new Color(0.6588235f, 0.2862745f, 0.01176471f, 1f);
+            return brownColor;
+        }
+    }
+    public static Color Y_LBrown
+    {
+        get
+        {
+            Color lBrownColor = new Color(0.8784314f, 0.6352941f, 0.3254902f, 1f);
+            return lBrownColor;
+        }
+    }
+
+
+
+
+
+
+    public static Color Green
+    {
+        get
+        {
+            Color greenColor = new Color(0.1164519f, 0.8301887f, 0f, 1f);
+            return greenColor;
+        }
+    }
+    public static Color G_DGreen
+    {
+        get
+        {
+            Color darkgreenColor = new Color(0f, 0.3921569f, 0f, 1f);
+            return darkgreenColor;
+        }
+    }
+    public static Color G_LGreen
+    {
+        get
+        {
+            Color superLightGreenColor = new Color(0.6939832f, 0.8679245f, 0.0286579f, 1f);
+            return superLightGreenColor;
+        }
+    }
+    public static Color G_SLGreen
+    {
+        get
+        {
+            Color lightGreenColor = new Color(0.7333333f, 0.9882353f, 0.3960784f, 1f);
+            return lightGreenColor;
+        }
+    }
+    public static Color G_SSLGreen
+    {
+        get
+        {
+            Color superLightGreenColor ;
+            if (ColorUtility.TryParseHtmlString("#E8FFC2", out superLightGreenColor))
+            {
+            }
+            return superLightGreenColor;
+        }
+    }
+
+
+
+
+
+    public static Color WB_LGray
+    {
+        get
+        {
+            Color lightGrayColor = new Color(0.6f, 0.6f, 0.6f, 1f);
+            return lightGrayColor;
+        }
+    }
+    public static Color WB_SLGray
+    {
+        get
+        {
+            Color superLightGrayColor = new Color(0.8f, 0.8f, 0.8f, 1f);
+            return superLightGrayColor;
+        }
+    }
+
+    public static Color HalfOpacityWhite
+    {
+        get
+        {
+            Color halfOpacityColor = new Color(1f, 1f, 1f, 0.5f);
+            return halfOpacityColor;
+        }
+    }
+    public static Color QuarterOpacityWhite
+    {
+        get
+        {
+            Color halfOpacityColor = new Color(1f, 1f, 1f, 0.25f);
+            return halfOpacityColor;
+        }
+    }
+    public static Color HalfOpacityBlack
+    {
+        get
+        {
+            Color halfOpacityColor = new Color(0f, 0f, 0f, 0.5f);
+            return halfOpacityColor;
+        }
+    }
+    public static Color QuarterOpacityBlack
+    {
+        get
+        {
+            Color halfOpacityColor = new Color(0f, 0f, 0f, 0.25f);
+            return halfOpacityColor;
+        }
+    }
+
+    #endregion
+
+    #region Favorite Colors
+
+    public static Color BaseWhite
+    {
+        get
+        {
+            Color SuperDarkColor;
+            if (ColorUtility.TryParseHtmlString("#FCFCFC", out SuperDarkColor))
+            {
+            }
+            return SuperDarkColor;
+        }
+    }
+
+    //Black  
+    public static Color BlackGray
+    {
+        get
+        {
+            Color BlackGray;
+            if (ColorUtility.TryParseHtmlString("#333333", out BlackGray))
+            {
+            }
+            return BlackGray;
+        }
+    }
+
+    
+    public static Color SetRandomColor(Color currentColor)
+    {
+        Color halfOpacityColor;
+
+        halfOpacityColor = new Color(currentColor.r, currentColor.g, currentColor.b, 0.5f);
+
+        return halfOpacityColor;
+    }
+    public static Color SetCurrentToHalfOpacity(Color currentColor)
+    {
+        Color halfOpacityColor;
+
+        halfOpacityColor = new Color(currentColor.r, currentColor.g, currentColor.b, 0.5f);
+
+        return halfOpacityColor;
+    }
+
+    public static Color SetNewColor(string colorHEXName)
+    {
+        Color SuperDarkColor;
+        if (ColorUtility.TryParseHtmlString(colorHEXName, out SuperDarkColor))
+        {
+        }
+        return SuperDarkColor;
+    }
+    #endregion
+
+
+
+
+
+
+
+
+
+}//EndClasssss
